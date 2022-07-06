f759182273d78ececfa3f26cd0f47f70612e8735
Nuri <67746320+NuriaROrquin@users.noreply.github.com>
2022-07-06 19:19:58 -0300

    agregue la tipografia

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#73A9AD",
+        teal: "#90C8AC",
+      },
+      backgroundImage: (theme) => ({
+        home: "url('/public/assets/banner-home.jpg')",
+      }),
+      fontFamily: {
+        "cairo-bold": "cairo-bold",
+        "cairo-semibold": "cairo-semibold",
+        "cairo-regular": "cairo-regular",
+        "cairo-light": "cairo-light",
+      },
+    },
   },
   plugins: [],
 };
