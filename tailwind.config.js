ab99fb64bf1264c447f4e63a4473833cf3d66196
Nuri <67746320+NuriaROrquin@users.noreply.github.com>
2022-07-06 22:29:09 -0300

    configure los banners en tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,12 @@ module.exports = {
         blue: "#73A9AD",
         teal: "#90C8AC",
       },
-      backgroundImage: (theme) => ({
+      backgroundImage: () => ({
         home: "url('/public/assets/banner-home.jpg')",
+        turnos: "url('/public/assets/banner-turnos.png')",
+        alimentacion: "url('/public/assets/banner-alimentacion.png')",
+        login: "url('/public/assets/banner-login.png')",
+        metricas: "url('/public/assets/banner-metricas.png')",
       }),
       fontFamily: {
         "cairo-bold": "cairo-bold",
@@ -15,7 +19,13 @@ module.exports = {
         "cairo-regular": "cairo-regular",
         "cairo-light": "cairo-light",
       },
+      transformOrigin: {
+        0: "0%",
+      },
     },
   },
   plugins: [],
+  variants: {
+    color: ["hover"],
+  },
 };
