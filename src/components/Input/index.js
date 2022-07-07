999955baec08d6d44058024dfd9c0eacef2f4fb9
Nuri <67746320+NuriaROrquin@users.noreply.github.com>
2022-07-06 22:30:07 -0300

    componente input

---
+++ src/components/Input/index.js
@@ -0,0 +1,35 @@
+export function Input({
+  id,
+  placeholder = " ",
+  label,
+  required,
+  type,
+  step,
+  tabIndex,
+  value,
+  disabled,
+  ariaLabel,
+}) {
+  return (
+    <div className="form-group">
+      <div>
+        <input
+          id={id}
+          type={type ?? "text"}
+          name={id}
+          step={step}
+          placeholder={placeholder}
+          tabIndex={tabIndex}
+          autoComplete="off"
+          defaultValue={value}
+          disabled={disabled ?? false}
+          onClick={(event) => event.target.select()}
+          aria-label={ariaLabel}
+        />
+        <label htmlFor={id} className={required ? "required" : ""}>
+          {label}
+        </label>
+      </div>
+    </div>
+  );
+}
