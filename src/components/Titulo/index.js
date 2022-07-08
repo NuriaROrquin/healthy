a8b9a4807c1a871a8234239527fb89e3f6b6b33a
Nuri <67746320+NuriaROrquin@users.noreply.github.com>
2022-07-08 17:40:47 -0300

    agregado del responsive login

--- src/components/Titulo/index.js
+++ src/components/Titulo/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Titulo({ texto, color }) {
-  return <h1 className={`${color}`}>{texto}</h1>;
+  return <h1 className={`${color} text-center py-4`}>{texto}</h1>;
 }
