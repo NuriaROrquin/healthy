5ad8454a0143903583b32ba3a84bdbe59ed9ca09
pablooantunez@hotmail.com <pablooantunez@hotmail.com>
2022-07-09 15:01:57 -0300

    Agregue cards con descripcion y funcionalidades

--- src/routes/turnos.js
+++ src/routes/turnos.js
@@ -11,24 +11,64 @@ export default function Turnos() {
       <BannerContainer titulo="Turnos" background="bg-turnos bg-center" />
       <h1>Turnos</h1>
       <SectionContainer>
-        <h3 className="text-center mb-24 text-3xl">
-          Qué estas esperando? Reservá un turno y comenzá ya a sentirte más
-          saludable!{" "}
+        <h3 className="text-center mb-24 text-3xl ">
+          ¡Conocé todas las actividades que tenemos para vos!
         </h3>
-        <div className="flex justify-around flex-wrap gap-2">
-         <Card image="https://img.freepik.com/vector-premium/mano-que-sostiene-mancuerna-conjunto-mancuernas-dobladas-normales-deformadas-blanco-equipo-deportivo-levantamiento-pesas-ejercicio-fuerza-concepto-gimnasio-estilo-ilustracion_257455-168.jpg?w=996" description="Clases grupales con tutor." title="SPINNING"/>
+        <div className="flex justify-around flex-wrap gap-2 gap-y-24">
+          <Card
+            image="/assets/spinning.jpg"
+            description="Clases grupales con tutor. Conocé todas las sedes disponibles y elegí la que más te guste."
+            title="SPINNING"
+          />
 
-         <Card image="https://img.freepik.com/vector-premium/mano-que-sostiene-mancuerna-conjunto-mancuernas-dobladas-normales-deformadas-blanco-equipo-deportivo-levantamiento-pesas-ejercicio-fuerza-concepto-gimnasio-estilo-ilustracion_257455-168.jpg?w=996" description="Clases grupales con tutor." title="RUNNING"/>
+          <Card
+            image="/assets/running.jpg"
+            description="Salí en grupo a recorrer las distintas calles de Buenos Aires."
+            title="RUNNING"
+          />
 
-         <Card image="https://img.freepik.com/vector-premium/mano-que-sostiene-mancuerna-conjunto-mancuernas-dobladas-normales-deformadas-blanco-equipo-deportivo-levantamiento-pesas-ejercicio-fuerza-concepto-gimnasio-estilo-ilustracion_257455-168.jpg?w=996" description="Clases grupales con tutor." title="CROSSFIT"/>
+          <Card
+            image="/assets/crossfit.webp"
+            description="Clases grupales con tutor."
+            title="CROSSFIT"
+          />
 
-         <Card image="https://img.freepik.com/vector-premium/mano-que-sostiene-mancuerna-conjunto-mancuernas-dobladas-normales-deformadas-blanco-equipo-deportivo-levantamiento-pesas-ejercicio-fuerza-concepto-gimnasio-estilo-ilustracion_257455-168.jpg?w=996" description="Clases grupales con tutor." title="FUNCIONAL"/>
+          <Card
+            image="/assets/funcional.png"
+            description="Hacé ejercicio sólo con tu peso! Clases grupales con tutor."
+            title="FUNCIONAL"
+          />
 
+          <Card
+            image="/assets/natacion.webp"
+            description="Clases grupales o individuales con tutor."
+            title="NATACION"
+          />
+
+          <Card
+            image="/assets/aquagym.webp"
+            description="Clases grupales con tutor para todas las edades. "
+            title="AQUAGYM"
+          />
+
+          <Card
+            image="/assets/yoga.jpg"
+            description="Clases grupales o individuales con tutor."
+            title="YOGA"
+          />
+
+          <Card
+            image="/assets/zumba.jpg"
+            description="Vení a bailar y divertirte en grupo con los mejores profesores del país. Conocé los lugares disponibles."
+            title="ZUMBA"
+          />
+          <h3 className="text-center mb-24 text-3xl ">
+            Qué estas esperando? Reservá un turno y comenzá ya a sentirte más
+            saludable!
+          </h3>
         </div>
-        
       </SectionContainer>
-
-      
+      <SectionContainer></SectionContainer>
     </>
   );
 }
