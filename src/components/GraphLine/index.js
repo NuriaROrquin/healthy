3f413902ae16a9d65897ba9eacc2118d58cac7a0
Nuri <67746320+NuriaROrquin@users.noreply.github.com>
2022-07-09 15:55:58 -0300

    implementacion de grafico

--- src/components/GraphLine/index.js
+++ src/components/GraphLine/index.js
@@ -1,21 +1,83 @@
-import { Chart } from "chart.js";
 import React from "react";
+import HighchartsReact from "highcharts-react-official";
+import HighchartsStock from "highcharts/highstock";
 
-export default function GraphLine() {
+export default function GraphLine({ titulo, series }) {
+  const options = {
+    chart: {
+      height: 500,
+      padding: [2, 2, 2, 2],
+    },
+    xAxis: {
+      type: "linear",
+      labels: {
+        enabled: true,
+      },
+    },
+    yAxis: {
+      title: {
+        text: "Cantidad de Clases",
+      },
+    },
+    title: {
+      text: titulo,
+    },
+    credits: {
+      enabled: false,
+    },
+    tooltip: {
+      headerFormat: '<span style="font-size:10px"></span><table>',
+      pointFormat:
+        '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+        '<td style="padding:0"><b> {point.y:.1f}</b></td></tr>',
+      footerFormat: "</table>",
+      shared: true,
+      useHTML: true,
+    },
+    legend: {
+      layout: "vertical",
+      align: "center",
+      verticalAlign: "bottom",
+    },
+    plotOptions: {
+      series: {
+        showInLegend: false,
+        pointIntervalUnit: "month",
+        line: {
+          color: "#90C8AC",
+          width: 3,
+        },
+      },
+    },
+    series: series,
+    rangeSelector: {
+      buttons: [
+        {
+          type: "month",
+          count: 1,
+          text: "1m",
+        },
+        {
+          type: "month",
+          count: 3,
+          text: "3m",
+        },
+        {
+          type: "all",
+          text: "All",
+        },
+      ],
+    },
+  };
+
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          height: "100%",
-          width: "30%",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
-        Chart
-      </div>
+      <HighchartsReact
+        highcharts={HighchartsStock}
+        options={options}
+        constructorType={"stockChart"}
+        containerProps={{ style: { width: "100%", height: "100%" } }}
+      />
     </>
   );
 }
