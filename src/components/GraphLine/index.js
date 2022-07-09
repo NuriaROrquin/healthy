10605d9d835a39b8c40d95b117c1da97e524324f
Nuri <67746320+NuriaROrquin@users.noreply.github.com>
2022-07-09 14:04:40 -0300

    correccion class a classname

---
+++ src/components/GraphLine/index.js
@@ -0,0 +1,21 @@
+import { Chart } from "chart.js";
+import React from "react";
+
+export default function GraphLine() {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          height: "100%",
+          width: "30%",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        Chart
+      </div>
+    </>
+  );
+}
--- src/routes/metricas.js
+++ src/routes/metricas.js
@@ -3,25 +3,30 @@ import Nav from "../components/Nav";
 import BannerContainer from "../components/BannerContainer";
 import SectionContainer from "../components/SectionContainer";
 import Card from "../components/card";
+import GraphLine from "../components/GraphLine";
 
 export default function Metricas() {
   return (
     <>
       <Nav />
       <BannerContainer titulo="Métricas" background="bg-metricas" />
-     
+
       <SectionContainer>
+        <h1>Metricas</h1>
 
-      <h1>Metricas</h1>
+        <div className="flex justify-around">
+          <div>
+            <Card />
+          </div>
+          <div>
+            <Card />
+          </div>
+          <div>
+            <Card />
+          </div>
+        </div>
 
-      <div class ="flex justify-around">
-
-        <div><Card /></div>
-        <div><Card /></div>
-        <div><Card /></div>
-
-      </div>
-
+        <GraphLine />
       </SectionContainer>
     </>
   );
