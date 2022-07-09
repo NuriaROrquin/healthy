d99638a2b23f9e018bbb532d110ec9ee10f00648
EugenioMIere <emierearrua@alumno.unlam.edu.ar>
2022-07-09 16:18:40 -0300

    implementacion de carrousel

--- src/routes/futuros.js
+++ src/routes/futuros.js
@@ -1,5 +1,48 @@
 import React from "react";
+import slick from "../components/slick";
+import BannerContainer from "../components/BannerContainer";
+import Card from "../components/card";
+import Nav from "../components/Nav";
+import SectionContainer from "../components/SectionContainer";
+import Slider from "react-slick";
 
 export default function Futuros() {
-  return <h1>Futuros</h1>;
+  return (
+    <>
+      <Nav />
+      <BannerContainer titulo="Turnos" background="bg-turnos bg-center" />
+      <h1>Turnos</h1>
+      <SectionContainer>
+        <h3 className="text-center mb-24 text-3xl">
+          Qué estas esperando? Reservá un turno y comenzá ya a sentirte más
+          saludable!{" "}
+        </h3>
+        
+        <Slider>
+        <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+            
+        </Slider>
+        
+
+        
+      </SectionContainer>
+    </>
+  );
 }
