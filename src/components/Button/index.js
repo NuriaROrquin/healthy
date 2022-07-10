b97731404d41fb8687997c766ac2540028539a7e
Nuri <67746320+NuriaROrquin@users.noreply.github.com>
2022-07-10 19:15:03 -0300

    update agendar turno

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,15 +2,21 @@ import React from "react";
 
 export default function ButtonWhite({ children, disabled }) {
   return (
-    <button className="px-6 py-2 mt-4 bg-white rounded-md text-teal max-w-xs hover:bg-tealLight hover:text-white transition disabled:bg-gray-300 disabled:text-white">
+    <button
+      disabled={disabled}
+      className="px-6 py-2 mt-4 bg-white rounded-md text-teal max-w-xs hover:bg-tealLight hover:text-white transition disabled:bg-gray-300 disabled:text-white"
+    >
       {children}
     </button>
   );
 }
 
-export function ButtonGreen({ children, disabled }) {
+export function ButtonGreen({ children, className, disabled }) {
   return (
-    <button className="px-6 py-2 mt-4 bg-teal rounded-md text-white max-w-xs hover:bg-white hover:border hover:border-teal hover:text-teal transition disabled:bg-gray-300 disabled:text-white">
+    <button
+      disabled={disabled}
+      className={`px-6 py-2 mt-4 bg-teal rounded-md text-white max-w-xs hover:bg-white hover:border hover:border-teal hover:text-teal transition disabled:bg-gray-300 disabled:text-white ${className}`}
+    >
       {children}
     </button>
   );
