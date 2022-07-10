e724ba1bf1e1028560bc7b0177ccbdccbb5b0f17
Nuri <67746320+NuriaROrquin@users.noreply.github.com>
2022-07-10 19:17:59 -0300

    mapeo deleted

--- src/index.js
+++ src/index.js
@@ -24,7 +24,6 @@ root.render(
         <Route path="alimentacion" element={<Alimentacion />} />
         <Route path="nivel" element={<Nivel />} />
         <Route path="futuros" element={<Futuros />} />
-        <Route path="mapeo" element={<Mapeo />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
