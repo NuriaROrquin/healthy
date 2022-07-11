c90e63835e9c589087fe4c6f56715762121649f8
Nuri <67746320+NuriaROrquin@users.noreply.github.com>
2022-07-11 18:55:53 -0300

    cambie el background

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Nav() {
   return (
     <nav className="flex py-4 px-4 justify-center bg-greenAqua w-full fixed z-50 mb-4">
-      <ul className="flex w-3/4 justify-around text-white font-cairo-semibold">
+      <ul className="flex w-full justify-around items-center text-white font-cairo-semibold">
         <li>
           <a href="/home">Home</a>
         </li>
@@ -12,6 +12,15 @@ export default function Nav() {
         </li>
         <li>
           <a href="/turnos">Turnos</a>
+        </li>
+        <li>
+          <a href="/user">
+            <img
+              src="/assets/user.jpeg"
+              alt="user"
+              className="w-12 h-12 object-cover rounded-full"
+            />
+          </a>
         </li>
       </ul>
     </nav>
